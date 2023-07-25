54c92ad550e0b987df194ce4ee26b01346d3bb74
chenht2010 <chenht2010@hotmail.com>
2023-07-25 11:05:23 +0800

    Initial commit

---
+++ project/lit_mnist.py
@@ -0,0 +1,78 @@
+from argparse import ArgumentParser
+
+import lightning as L
+from lightning.pytorch.cli import LightningCLI
+import torch
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from torchvision.datasets.mnist import MNIST
+
+
+class LitClassifier(L.LightningModule):
+    def __init__(self, hidden_dim=128, learning_rate=1e-3):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.l1 = torch.nn.Linear(28 * 28, self.hparams.hidden_dim)
+        self.l2 = torch.nn.Linear(self.hparams.hidden_dim, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.l1(x))
+        x = torch.relu(self.l2(x))
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log("valid_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log("test_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+
+
+class MNISTDataModule(L.LightningDataModule):
+    def __init__(self, data_dir: str = "path/to/dir", batch_size: int = 32):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+
+    def setup(self, stage: str):
+        self.mnist_test = MNIST(self.data_dir, train=False)
+        self.mnist_predict = MNIST(self.data_dir, train=False)
+        mnist_full = MNIST(self.data_dir, train=True)
+        self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=self.batch_size)
+
+    def predict_dataloader(self):
+        return DataLoader(self.mnist_predict, batch_size=self.batch_size)
+
+
+def cli_main():
+    cli = LightningCLI(LitClassifier, MNISTDataModule)
+
+
+if __name__ == "__main__":
+    cli_main()
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='project',
+    version='0.0.0',
+    description='Describe Your Cool Project',
+    author='',
+    author_email='',
+    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url='https://github.com/chenht2010/lightning-project-template',
+    install_requires=['lightning'],
+    packages=find_packages(),
+)
+
